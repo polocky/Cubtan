14776cadbf4123f37afe7e75fe1695a5701554a6
polocky <teranishi@livedoor.jp>
2010-07-04 17:06:42 +0900

    initial commit

---
+++ lib/Log/SpeedAnalyze.pm
@@ -0,0 +1,27 @@
+package Log::SpeedAnalyze;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Log::SpeedAnalyze -
+
+=head1 SYNOPSIS
+
+  use Log::SpeedAnalyze;
+
+=head1 DESCRIPTION
+
+Log::SpeedAnalyze is
+
+=head1 AUTHOR
+
+Default Name E<lt>default {at} example.comE<gt>
+
+=head1 SEE ALSO
+
+=cut
---
+++ t/00_load_all.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+use Test::More;
+
+BEGIN {
+      use_ok( 'Log::SpeedAnalyze' );
+}
+
+done_testing();
